fc9d720c4ec2ab33cc4c36aeb2f4c6edaeaed3f1
ccobb1 <71273360+ccobb1@users.noreply.github.com>
2020-11-26 11:09:35 -0500

    refactoring, updated sample_midi_track and piano_roll

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,27 @@ import numpy as np
 
 # https://mido.readthedocs.io/en/latest/
 
+def get_files(data_dir):
+    """
+    :param data_dir: string, name of directory containing midi files
+    :return: a list of midi files in that directory
+    """
+    midi_files = []
+    for file in os.listdir(data_dir):
+        if file.endswith(".mid"):
+            path = os.path.join(data_dir, file)
+            #score = music21.converter.parse(path) #Error: 'badly formed midi string: missing leading MTrk'
+            #key = score.analyze('key')
+            #print(key)
+            try:
+                midi_files.append(MidiFile(path))
+                print(path)
+            except OSError:
+                continue
+            except EOFError:
+                continue
+    return midi_files
+
 def normalize(midi_file):
     """
     this should take a single midi track and normalize it so that it is in C and the tempos
@@ -28,27 +49,10 @@ def normalize(midi_file):
     """tempo = int((60.0 / bpm) * 1000000)
     track.append(MetaMessage('set_tempo', tempo=tempo))"""
 
-    return midi
+    return midi_file
     # return sNew #music21.midi.translate.streamToMidiFile(sNew)
 
-
-data_dir = 'data/Bach/Fugue'
-
-midi_files = []
-
-for file in os.listdir(data_dir):
-    if file.endswith(".mid"):
-        path = os.path.join(data_dir, file)
-        # score = music21.converter.parse(path) #Error: 'badly formed midi string: missing leading MTrk'
-        # key = score.analyze('key')
-        # print(key)
-        try:
-            midi_files.append(MidiFile(path))
-        except OSError:
-            continue
-        except EOFError:
-            continue
-
+"""
 max_tracks = 0
 for file in midi_files:
     print("Processing file ", file)
@@ -67,6 +71,7 @@ for file in midi_files:
                     msg = MetaMessage('set_tempo', tempo=tempo)
                 print(msg)
             continue
+"""
 
 
 def sample_midi_track(track, interval, num_samples):
@@ -80,28 +85,24 @@ def sample_midi_track(track, interval, num_samples):
     interval ticks
     """
 
-    samples = np.zeros((num_samples,))
-    sample_time = 0
-
-    msg_index = 1  # index of the first "on" message
-    note_start_time = track[msg_index].time
-    note_duration = track[msg_index + 1].time - note_start_time
-
-    while msg_index < len(track) - 3:
-
-        while sample_time < note_start_time:
-            samples[int(sample_time / interval)] = None  # there is no note playing
-            sample_time += interval
-
-        while sample_time < note_start_time + note_duration:
-            if note_start_time <= sample_time < note_start_time + note_duration:
-                samples[int(sample_time / interval)] = track[msg_index].note
-                sample_time += interval
-
-        msg_index += 2  # go to the next note ON message
-        note_start_time += note_duration + track[msg_index].time
-        note_duration = track[msg_index+1].time
-
+    cur_time = 0
+    next_msg_time = 0
+    arr_index = 0
+    samples = np.zeros(num_samples)
+    msgs = track[1:] # exclude track[0], which is metadata
+    
+    for msg in msgs:
+        next_msg_time += msg.time
+        while cur_time < next_msg_time:
+            # if msg.type=='note_off', add the note (since this means it's
+            # currently on and will be turned off at next_msg_time).
+            # otherwise, it remains 0 (i.e., the note is off)
+            if msg.type=='note_off':
+                samples[arr_index] = msg.note
+            cur_time += interval
+            arr_index += 1
+        
+    print(samples[len(samples)-20:])
     return samples
 
 
@@ -111,29 +112,31 @@ def piano_roll(midi_file):
     :param midi_file:
     :return:
     """
-    num_tracks = len(midi_file.tracks)
-    tracks = midi_file.tracks[1:num_tracks]  # drop the metadata track
+    tracks = midi_file.tracks[1:]  # drop the metadata track
+    ticks_per_eighth_note = midi_file.ticks_per_beat / 2  # ticks per eighth note
+    print(ticks_per_eighth_note)
+    
+    # TODO: we want no more than 3 tracks, so if there are more, randomly choose 3 of them to work with
+    
+    # the tracks seem to be slightly different lengths for some reason, so take
+    # the max length to determine the number of samples
+    for track in tracks:
+        print(sum([m.time for m in track]))
+    
+    max_length = max([sum([m.time for m in track]) for track in tracks])
+    num_samples = np.ceil(max_length / ticks_per_eighth_note).astype('int')
 
-    ticks_per_beat = midi_file.ticks_per_beat
-    ticks_per_eighth_note = ticks_per_beat / 8  # ticks per eighth note
-
-    # all tracks should have the same length
-    track_length = sum([m.time for m in tracks[0]])
-    num_samples = int(track_length / ticks_per_eighth_note)
-
-    piano_roll = []
-    for t in tracks:
-        sampled_track = sample_midi_track(t, ticks_per_eighth_note, num_samples)
-        piano_roll.append(sampled_track)
-
+    piano_roll = np.array([sample_midi_track(track, ticks_per_eighth_note, num_samples) for track in tracks])
+    
     # TODO concatenate the samples from all tracks into a single list of tokens where each
     #  token represents all notes being played at the current tick
 
     # ie somehow flatten the first dimension of piano_roll by combining note values
 
-    piano_roll = np.stack(piano_roll)
-    assert(np.array_equal(np.shape(piano_roll), (num_tracks-1, num_samples)))
+    print(piano_roll.shape)
     return piano_roll
 
 
-pr = piano_roll(midi_files[0])
+midi_files = get_files('data/Bach/Fugue')
+for midi_file in midi_files:
+    piano_roll(midi_file)
